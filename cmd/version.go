93dfa5eb42e8718e63b9273e173e6b2c2db0d9ff
totegamma <github@gammalab.net>
2025-06-01 01:17:14 +0900

    add version cmd

---
+++ cmd/version.go
@@ -0,0 +1,33 @@
+package cmd
+
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+	"runtime/debug"
+)
+
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Print the version number of the application",
+	Long:  `All software has versions. This is the version of this application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+
+		buildInfo, ok := debug.ReadBuildInfo()
+		if !ok {
+			fmt.Println("Build information is not available.")
+			return
+		}
+
+		fmt.Printf("Version: %s\n", buildInfo.Main.Version)
+		fmt.Printf("Go Version: %s\n", buildInfo.GoVersion)
+		fmt.Printf("Build Settings:\n")
+		for _, setting := range buildInfo.Settings {
+			fmt.Printf("  %s: %s\n", setting.Key, setting.Value)
+		}
+
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(versionCmd)
+}
