3fcaed7d4a669253e22b9df4c55c16c476f3ead9
totegamma <github@gammalab.net>
2025-08-07 22:15:04 +0900

    update yaml lib

--- lib/yaml.go
+++ lib/yaml.go
@@ -4,13 +4,15 @@ import (
 	"fmt"
 
 	"github.com/totegamma/yisp/core"
+	"github.com/totegamma/yisp/internal/yaml"
 )
 
 func init() {
-	register("yaml", "marshal", opToYaml)
+	register("yaml", "marshal", opMarshal)
+	register("yaml", "unmarshal", opUnmarshal)
 }
 
-func opToYaml(cdr []*core.YispNode, env *core.Env, mode core.EvalMode, e core.Engine) (*core.YispNode, error) {
+func opMarshal(cdr []*core.YispNode, env *core.Env, mode core.EvalMode, e core.Engine) (*core.YispNode, error) {
 	if len(cdr) != 1 {
 		return nil, core.NewEvaluationError(nil, fmt.Sprintf("toYaml requires 1 argument, got %d", len(cdr)))
 	}
@@ -26,4 +28,32 @@ func opToYaml(cdr []*core.YispNode, env *core.Env, mode core.EvalMode, e core.En
 		Value: yamlStr,
 		Attr:  node.Attr,
 	}, nil
+}
+
+func opUnmarshal(cdr []*core.YispNode, env *core.Env, mode core.EvalMode, e core.Engine) (*core.YispNode, error) {
+	if len(cdr) != 1 {
+		return nil, core.NewEvaluationError(nil, fmt.Sprintf("fromYaml requires 1 argument, got %d", len(cdr)))
+	}
+	node := cdr[0]
+	if node.Kind != core.KindString {
+		return nil, core.NewEvaluationError(node, fmt.Sprintf("fromYaml requires a string argument, got %v", node.Kind))
+	}
+
+	yamlStr, ok := node.Value.(string)
+	if !ok {
+		return nil, core.NewEvaluationError(node, fmt.Sprintf("invalid yaml string value: %T", node.Value))
+	}
+
+	var result any
+	err := yaml.Unmarshal([]byte(yamlStr), &result)
+	if err != nil {
+		return nil, core.NewEvaluationErrorWithParent(node, "failed to unmarshal yaml", err)
+	}
+
+	resultNode, err := core.ParseAny(node.Attr.File, result)
+	if err != nil {
+		return nil, core.NewEvaluationErrorWithParent(node, "failed to parse yaml result", err)
+	}
+
+	return resultNode, nil
 }
